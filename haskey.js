12514d10d8f55a82f8552709a6d013a884bc6fe1
王恺伦 <wangkailun@wangkailundeMBP.fios-router.home>
2020-08-15 12:06:28 -0400

    object key

---
+++ haskey.js
@@ -0,0 +1,26 @@
+const hasKey = (obj, keys) => {
+    return (
+        keys.length > 0 && 
+        keys.every(key => {
+            if(typeof obj !== 'object' || !obj.hasOwnProperty(key)) {
+                return false;
+            }
+            obj = obj[key];
+            return true;
+        })
+    )
+}
+
+let obj = {
+    a: 1,
+    b: { c: 4 },
+    'b.d': 5
+  };
+console.log(hasKey(obj, ['b']));
+//   hasKey(obj, ['a']); // true
+//   hasKey(obj, ['b']); // true
+//   hasKey(obj, ['b', 'c']); // true
+//   hasKey(obj, ['b.d']); // true
+//   hasKey(obj, ['d']); // false
+//   hasKey(obj, ['c']); // false
+//   hasKey(obj, ['b', 'f']); // false
---
+++ omitBy.js
@@ -0,0 +1,7 @@
+const omitBy = (obj, fn) =>
+  Object.keys(obj)
+    .filter(k => !fn(obj[k]))
+    .reduce((acc, key) => ((acc[key] = obj[key]), acc), {});
+
+
+console.log(omitBy({ a: 1, b: '2', c: 3 }, x => typeof x === 'number'));
